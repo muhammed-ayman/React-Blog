4e54beeb8997169e44c859ad9f86b0fb28d496a7
Mohammed ayman <mae252002@gmail.com>
2021-09-10 14:53:01 +0200

    added the initial release files

---
+++ src/BlogDetails.js
@@ -0,0 +1,34 @@
+import { useParams, useHistory } from "react-router-dom";
+import useFetch from './useFetch';
+
+const BlogDetails = () => {
+    const { id } = useParams();
+    const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+    const history = useHistory();
+
+    const handleDelete = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE',
+
+        }).then(() => {
+            history.push('/');
+        })
+    }
+
+    return (
+        <div className="blog-details">
+            { isPending && <p>Loading...</p> }
+            { error && <p>{error}</p>}
+            { blog && (
+                <article>
+                    <h2>{ blog.title }</h2>
+                    <p>Written By: { blog.author }</p>
+                    <div>{ blog.body }</div>
+                    <button onClick={handleDelete}>Delete</button>
+                </article>
+            )}
+        </div>
+    );
+}
+ 
+export default BlogDetails;
